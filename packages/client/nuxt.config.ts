3bf0106392dbb5f93db1484c62dc8568caec7fe2
velvetcode206 <77030734+velvetcode206@users.noreply.github.com>
2025-08-08 00:17:29 -0300

    feat(infra/client): added metadada to clientm, fixed typo on cd workflow

--- packages/client/nuxt.config.ts
+++ packages/client/nuxt.config.ts
@@ -6,6 +6,25 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   typescript: { tsConfig: { extends: resolve(dirname(fileURLToPath(import.meta.url)), '../../tsconfig.base.json') } },
+  app: {
+    head: {
+      htmlAttrs: { lang: 'en-US' },
+      meta: [
+        { charset: 'utf-8' },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1.0',
+        },
+        { name: 'description', content: 'This is a template Nuxt4 application' },
+      ],
+      title: 'Client',
+      link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico',
+      }],
+    },
+  },
   modules: [
     '@nuxt/eslint',
   ],

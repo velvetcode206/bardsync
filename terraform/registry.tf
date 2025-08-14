1e663a32455f3df60429857a826bb02b3ff1f771
velvetcode206 <77030734+velvetcode206@users.noreply.github.com>
2025-08-14 00:51:12 -0300

    fix(infra/client): refactored cd workflow, updated terraform and client Dockerfile

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -3,4 +3,23 @@ resource "google_artifact_registry_repository" "client" {
   description   = var.gcp_artifact_repo_client_description
   format        = var.gcp_artifact_repo_client_format
   location      = var.gcp_region
+
+  cleanup_policies {
+    id     = "keep-minimum"
+    action = "KEEP"
+
+    most_recent_versions {
+      package_name_prefixes = ["client-prd", "client-dev"]
+      keep_count = 3
+    }
+  }
+
+  cleanup_policies {
+    id     = "delete-older"
+    action = "DELETE"
+
+    condition {
+      older_than = "0d"
+    }
+  }
 }

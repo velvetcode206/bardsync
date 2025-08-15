5fcb5e436c39afde114b6823474ac67f3a85cebc
velvetcode206 <77030734+velvetcode206@users.noreply.github.com>
2025-08-15 14:03:36 -0300

    fix(client): update service status  component visibility

--- packages/client/app/components/ServiceStatus.vue
+++ packages/client/app/components/ServiceStatus.vue
@@ -5,7 +5,7 @@ const { data } = await useFetch('/api/services-status')
 <template>
   <div class="services-status">
     <p v-for="service in data" :key="service.service">
-      <a :href="`http://34.54.212.41/services/${service.service}`" style="text-transform: capitalize; text-decoration: underline; color: cyan">{{ service.service }}:</a>
+      <a :href="`http://34.54.212.41/services/${service.service}`" style="text-transform: capitalize; text-decoration: underline; color: blue">{{ service.service }}:</a>
       {{ service.status }}
     </p>
   </div>
@@ -16,8 +16,9 @@ const { data } = await useFetch('/api/services-status')
   display: flex;
   flex-direction: column;
   gap: 8px;
+  padding: 32px 0;
   align-items: center;
   width: 100%;
-  color: white;
+  color: black;
 }
 </style>
